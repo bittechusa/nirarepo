a8e7b4eb3535c66828b4e7cccbcb97c031463f38
unknown <lowlyheart@.yahoo.com>
2015-01-17 19:25:48 -0500

    pro

--- ProjectLost/src/DropDownSize.java
+++ ProjectLost/src/DropDownSize.java
@@ -20,16 +20,17 @@ public class DropDownSize
 	@Test
 	public void m1() throws InterruptedException
 	{
-		FirefoxDriver dr= new FirefoxDriver();
+		FirefoxDriver dr=new FirefoxDriver();
+		/*FirefoxDriver dr= new FirefoxDriver();
 		dr.get("http:www.facebook.com");
-		/*dr.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// implicit wait
+		dr.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// implicit wait
 		dr.findElement(By.id("u_0_3"));// email box
 		WebDriverWait cd= (WebDriverWait) new WebDriverWait(dr, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("u_0_3")));// Explicit wait
 		System.out.println(cd);// explicit wait
 		cd.until(ExpectedConditions.elementToBeClickable(By.id("u_0_n")));//explicit wait
-		dr.findElement(By.id("u_0_n"));*/
+		dr.findElement(By.id("u_0_n"));
 		
-		
+		*/
 		
 		// HOW TO GET ALL TEXT LINKS IN AN WEBSITE.
 
